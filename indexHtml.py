0aa94a875bec775ce228d39c5752f39c6f304092
DartiLila <95767904+DartiLila@users.noreply.github.com>
2022-10-10 11:41:01 +0200

    Optimized the code and made the map fully functional!

--- indexHtml.py
+++ indexHtml.py
@@ -1,53 +1,17 @@
-from threading import Thread
 from ionoMapApp import app
 from ionoMapApp import server
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
-from mapp import *
-from PIL import Image
-
-
-def convertImage():
-
-    # Get Image and make it tansparent
-    img = Image.open("assets/Map.png")
-    img = img.convert("RGBA")
-
-    datas = img.getdata()
-
-    newData = []
-
-    for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
-            newData.append((255, 255, 255, 0))
-        else:
-            newData.append(item)
-
-    img.putdata(newData)
-    img.save("assets/NewMap.png", "PNG")
-    print("Successful")
-
-# Converts back to image
-convertImage()
-
-# Website design
-
-background = Image.open("assets/MapOfWorld.png")
-foreground = Image.open("assets/NewMap.png")
-
-background.paste(foreground, (0, 0), foreground)
+from map import *
 
 app.layout = html.Div([html.H1('IonoMap',
                                style={'color': 'white',
                                       'textAlign': 'center',
                                       'background': 'black'}),
-                       html.Img(src=app.get_asset_url('MapOfWorld.png'),height = 600,
+                       html.Img(src=app.get_asset_url('Map.png'), height=600,
                                 style={'margin-left': '335px',
-                                       'margin-right': '100px',}),
-                       html.Img(src=app.get_asset_url('Map.png'),height = 600,
-                                style={'margin-left': '335px',
-                                       'margin-right': '100px',})])
+                                       'margin-right': '100px', })])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- map.py
+++ map.py
@@ -3,18 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from netCDF4 import Dataset as NetCDFFile
 
-# Create the map
-map = Basemap(projection='kav7', lon_0=0, resolution='c')
-
-# Color the map acording to area (Continents green, ocans and lakes black)
-map.drawmapboundary(fill_color='black')
-map.fillcontinents(color='green', lake_color='black')
-map.drawcoastlines()
-
-# Create the meridians and parallels and name them
-map.drawparallels(np.arange(-90., 91., 30.), labels=[1, 1, 0, 1], color='white', textcolor='black')
-map.drawmeridians(np.arange(-180., 181., 60.), labels=[1, 1, 0, 1], color='white', textcolor='black')
-
 # Coordinates for space agencies: NASA Mission Control Centre: 29.560034, -95.089188
 #                         Australian Space Center: -34.920288, 138.608255
 #                         Brazilian Space Agency: -15.815123, -47.943437
@@ -34,10 +22,6 @@ lons = [-95.089188, 138.608255, -47.943437, -73.393664, 2.389846, 77.570820, 2.3
 lats = [29.560034, -34.920288, -15.815123, 45.520009, 48.843610, 13.035112, 48.873461, 19.360946, -34.616677, 26.224290,
         -25.293470, -34.424128]
 
-x, y = map(lons, lats)
-
-map.scatter(x, y, marker='o', color='m')
-
 
 # Loads the data file
 nc = NetCDFFile('/Users/User/Documents/GitHub/Ionomap/exampleData/dataset1.nc')
@@ -47,25 +31,42 @@ ionovar = nc.variables['percent_of_normal']
 data = 0.01 * ionovar[:]
 latcorners = nc.variables['y'][:]
 loncorners = -nc.variables['x'][:]
-#lon_0 = -nc.variables['normal'].getValue()
-#lat_0 = nc.variables['observation'].getValue()
+lon_0 = -nc.variables['normal'][:]
+lat_0 = nc.variables['observation'][:]
 
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
+# Create the map
+m = Basemap(projection='kav7', lon_0=0, resolution='c')
+
+# Color the map acording to area (Continents green, ocans and lakes black)
+m.drawmapboundary(fill_color='black')
+m.fillcontinents(color='green', lake_color='black')
+m.drawcoastlines()
+
+# Create the meridians and parallels and name them
+m.drawparallels(np.arange(-90., 91., 30.), labels=[1, 1, 0, 1], color='white', textcolor='black')
+m.drawmeridians(np.arange(-180., 181., 60.), labels=[1, 1, 0, 1], color='white', textcolor='black')
+
+x, y = m(lons, lats)
+
+m.scatter(x, y, marker='o', color='m')
+
 # Gets the specific coordinates and draws map
 ny = data.shape[0]
 nx = data.shape[1]
-lons, lats = map.makegrid(nx, ny)  # get lat/lons of ny by nx evenly space grid.
-xx, yy = map(lons, lats)  # compute map proj coordinates.
+lons, lats = m.makegrid(nx, ny)  # get lat/lons of ny by nx evenly space grid.
+xx, yy = m(lons, lats)  # compute map proj coordinates.
 
 
 # Map legend
 clevs = [0, 1, 2.5, 5, 7.5, 10, 15, 20, 30, 40, 50, 70, 100, 150, 200, 250, 300, 400, 500, 600, 750]
-cs = map.contourf(xx, yy, data, clevs, cmap=cm.s3pcpn)
+cs = m.contourf(xx, yy, data, clevs, cmap=cm.s3pcpn)
 
-cbar = map.colorbar(cs, location='bottom', pad="5%")
+cbar = m.colorbar(cs, location='bottom', pad="5%")
 cbar.set_label('AER')
 
 plt.title('Ionosphere Projection')
-plt.savefig("C:/Users/User/Documents/GitHub/Ionomap" + "/assets/Map.png")
+plt.savefig("C:/Users/User/Documents/GitHub/Ionomap/assets/Map.png")
+plt.show()
