cebb44167c523b4af84ddb8ee8702bda06321a75
DartiLila <95767904+DartiLila@users.noreply.github.com>
2022-10-12 15:54:25 +0200

    Added some exeptions(further testing required)

--- test.py
+++ test.py
@@ -7,11 +7,16 @@ class SpaceAgency:
     def getValues(self):
         print(self.name, self.longitude, self.latitude)
 
+
 class SpaceAgenciesManager:
     def __init__(self, fileName='C:/Users/User/Documents/GitHub/Ionomap/assets/SpaceAgencies.txt'):
         self.fileName = fileName
 
     def addAgency(self, name, longitude, latitude):
+        if not type(name) is str:
+            raise TypeError("Attribute 'name' must be string")
+        if not type(longitude) and type(latitude) is int or float:
+            raise TypeError("Attribute 'longitude' and 'latitude' must be int or float")
         with open(self.fileName, "a") as textFile:
             agency = f"\n{name}, {longitude}, {latitude}"
             textFile.write(agency)
@@ -31,18 +36,18 @@ class SpaceAgenciesManager:
     def agenciesObjCreator(self):
         with open(self.fileName, "r") as textFile:
             lines = textFile.readlines()
-            # return map(lambda line: parseAgency(line), lines)
             return map(self.parseAgency, lines)
 
     @staticmethod
     def parseAgency(line):
-        tokens = line.strip().split(", ")
+        tokens = line.strip().split(",")
         print(tokens)
-        return SpaceAgency(tokens[0],  float(tokens[1]), float(tokens[2]))
+        return SpaceAgency(tokens[0], float(tokens[1]), float(tokens[2]))
 
 
 if __name__ == "__main__":
-
     newAgency = SpaceAgenciesManager()
+    newAgency.addAgency("hep", 3, 5)
+
     # newAgency.addAgency("test", 12, 4)
-    newAgency.removeAgency("test")
+    # newAgency.removeAgency("test")
